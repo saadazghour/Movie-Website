797af59c19ced77abb149f5afe9821074eb9d7ff
AZGHOUR-SAAD <saad.azghour@gmail.com>
2018-09-11 16:20:30 +0100

    Update image source

--- MovieWebsite.py
+++ MovieWebsite.py
@@ -1,15 +1,16 @@
 import webbrowser
 
 class Movie(object):	
-	"""docstring for Movie == This Class Provide a Way To Store Movie Related Information"""
+	"""docstring for Movie == This Class Provide
+	 A Way To Store Movie Related Information"""
+
 	#VALID_RADING = ['Mohammed','AZ','SAAD','Azghour']
 	def __init__(self, movie_title,movie_story_line,poster_image,trailer_youtube):
 		self.title = movie_title
-		self.storyline = movie_story_line					# self is not a Python keyword
+		self.storyline = movie_story_line		# self is not a Python keyword
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
 		# super(Movie, self).__init__()
-		# self.arg = arg  
 
 		# def __init__(self,Best_Trailer):
 		# 	self.Best_Trailer = Best_Trailer
--- entertainmentCenter.py
+++ entertainmentCenter.py
@@ -1,11 +1,10 @@
 import MovieWebsite
 import fresh_tomatoes
-
 import webbrowser
 
 
 Toy_Story = MovieWebsite.Movie("Toy_Story","A Story of A Best Movie"
-	,r"https://www.google.com/imgres?imgurl=http%3A%2F%2Fyesofcorsa.com%2Fwp-content%2Fuploads%2F2017%2F02%2FToy-Story-Photo.jpg&imgrefurl=http%3A%2F%2Fyesofcorsa.com%2Ftoy-story%2F&docid=JgdrHqxr9kgWOM&tbnid=KZz5h8OlN_slRM%3A&vet=10ahUKEwiCy_mIhvnbAhVBpCwKHbgUD2cQMwgvKAAwAA..i&w=1024&h=768&bih=532&biw=1024&q=%22toy%20story%20image%20jpg%22&ved=0ahUKEwiCy_mIhvnbAhVBpCwKHbgUD2cQMwgvKAAwAA&iact=mrc&uact=8",
+	,r"https://www.google	.com/imgres?imgurl=http%3A%2F%2Fyesofcorsa.com%2Fwp-content%2Fuploads%2F2017%2F02%2FToy-Story-Photo.jpg&imgrefurl=http%3A%2F%2Fyesofcorsa.com%2Ftoy-story%2F&docid=JgdrHqxr9kgWOM&tbnid=KZz5h8OlN_slRM%3A&vet=10ahUKEwiCy_mIhvnbAhVBpCwKHbgUD2cQMwgvKAAwAA..i&w=1024&h=768&bih=532&biw=1024&q=%22toy%20story%20image%20jpg%22&ved=0ahUKEwiCy_mIhvnbAhVBpCwKHbgUD2cQMwgvKAAwAA&iact=mrc&uact=8",
 	"https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 
